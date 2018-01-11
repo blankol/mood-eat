160a37d7ec85335968caa5e8141f89acc75e8a87
Annette Coll <annette.coll@outlook.com>
2018-01-11 11:59:10 -0500

    updating db uri

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://annette.annette@ds251197.mlab.com:51197/moodeat-prod'}
+    module.exports = {mongoURI: 'mongodb://annette.annette123@ds251197.mlab.com:51197/moodeat-prod'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/mood-eat'}
 }
